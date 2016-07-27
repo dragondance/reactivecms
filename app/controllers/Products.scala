9e8d893fdbedd7de10cf8d51e6bb873fd7ed3baf
dragondance <=>
2016-07-27 20:10:08 +0200

    Fix call to play.api.data._ in products.scala.

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -3,8 +3,9 @@ package controllers
 import javax.inject._
 import play.api.mvc.{Action, Controller}
 import models.Product
-import play.adi.data._
+import play.api.data._
 import play.api.data.Forms._
+import play.api.i18n.Messages
 
 class Products @Inject() extends Controller{
   def list = Action { implicit request =>
@@ -18,4 +19,12 @@ class Products @Inject() extends Controller{
       Ok(views.html.products.details(product))
     }.getOrElse(NotFound)
   }
+
+  private val productForm: Form[Product] = Form(
+    mapping(
+      "ean" -> longNumber.verifying("validation.ean.duplicate", Product.findByEan(_).isEmpty),
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText
+    )(Product.apply)(Product.unapply)
+  )
 }
